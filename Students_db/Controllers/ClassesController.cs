26be7a47a5ed79aaaf30ea7870aa8e8ab3b9f126
Valery Gurov <va1ery@ya.ru>
2020-04-23 10:05:52 +0300

    Entity Classes created

---
+++ Students_db/Controllers/ClassesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Students_db.Models;
+
+namespace Students_db.Controllers
+{
+    public class ClassesController : Controller
+    {
+        private StudentsEntities db = new StudentsEntities();
+
+        // GET: Classes
+        public ActionResult Index()
+        {
+            return View(db.Classes.ToList());
+        }
+
+        // GET: Classes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Classes classes = db.Classes.Find(id);
+            if (classes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classes);
+        }
+
+        // GET: Classes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Classes/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ClassID,ClassName,DepartmentID,Section__,InstructorID,Term,Units,Year,Location,DaysAndTimes,Notes")] Classes classes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Classes.Add(classes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(classes);
+        }
+
+        // GET: Classes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Classes classes = db.Classes.Find(id);
+            if (classes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classes);
+        }
+
+        // POST: Classes/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ClassID,ClassName,DepartmentID,Section__,InstructorID,Term,Units,Year,Location,DaysAndTimes,Notes")] Classes classes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(classes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(classes);
+        }
+
+        // GET: Classes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Classes classes = db.Classes.Find(id);
+            if (classes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(classes);
+        }
+
+        // POST: Classes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Classes classes = db.Classes.Find(id);
+            db.Classes.Remove(classes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
